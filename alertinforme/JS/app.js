5a49b80e12a810cb24bcf61defe35ed7ef7825f3
WallingtonAB <wallington.bruno1@gmail.com>
2023-05-05 17:03:01 -0300

    atividade-iniciais

---
+++ alertinforme/JS/app.js
@@ -0,0 +1,17 @@
+let start = parseFloat(prompt("Type the first number:"));
+let startT = parseFloat(prompt("Type the second number:"));
+let show = alert("See the loop when you do an add!");
+let add = parseFloat(start + startT);
+let addd = parseFloat(start + startT);
+let anwers = alert(add);
+let anwerss = alert(addd);
+console.log(add);
+
+   while (add > 1) {
+    start = parseFloat(prompt("Type the first number:"));
+    startT = parseFloat(prompt("Type the second number:"));
+    addd = parseFloat(start + startT);
+    anwerss = alert(addd);
+    console.log(addd);
+    //alert("See the loop when you do an add!" + add++)
+   }
